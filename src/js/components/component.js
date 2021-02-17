1f06b036a14442f4d0c3b58668fd2e990c3c34b5
PingWin <grabcterplay@gmail.com>
2021-02-17 22:09:06 +1000

    v1.0 web tempalate

---
+++ src/js/components/component.js
@@ -0,0 +1,13 @@
+class Test {
+  constructor() {
+    this.text = 'Library Test connection on main files';
+  }
+
+  writeText() {
+    console.log('Connection');
+
+    console.log(this.text);
+  }
+}
+
+export default Test.prototype;
---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import '../scss/main.scss';
+import '../index.html';
+
+import Test from './components/component';
+
+console.log('Start main file');
+
+const test = Test;
+test.writeText();
+
+const a = 1;
+
+function foo(name) {
+  const lastName = name;
+}
+
+const x = 200;
+
+console.log(window);
+console.log(a);
